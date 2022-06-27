f3c21c82eff1f86ba71e80e0b81fd055640f779a
Jalilov <javohirjalilov777@gmail.com>
2022-06-27 21:59:28 +0500

    task four

--- bin/bool04.dart
+++ bin/bool04.dart
@@ -6,5 +6,14 @@
     Returns:
         bool
     */
+bool func(a){
+    return a < 0;
+}
 
-void main() {}
+void main() {
+    int a = -3;
+    bool answer;
+    answer = func(a);
+
+    print(answer);
+}
